616d6a3e2312f7fa9bc4f31a2a2b130c3456297d
Andy Lo <andy.lo@belointel.com>
2020-04-23 20:46:11 -0500

    Planet rendering experiments.

--- js/script.js
+++ js/script.js
@@ -1,59 +1,122 @@
-function generateCube() {
-  let geometry = new THREE.BoxGeometry(1, 1, 1);
-  let material = new THREE.MeshBasicMaterial( { color: 0x0000ff });
-  let mesh = new THREE.Mesh(geometry, material);
+let materialName = "earth";
 
-  return mesh;
+const stellarClassification = {
+  O: 0x9db4ff,
+  B: 0xaabfff,
+  A: 0xcad8ff,
+  F: 0xf8f7ff,
+  G: 0xfff2a1,
+  K: 0xffeecc,
+  M: 0xffd2a1,
+};
+
+async function loadTextures() {
+  const diffuseMapDirPath = "assets/diffuse-maps";
+  const bumpMapDirPath = "assets/bump-maps";
+  const specularMapDirPath = "assets/specular-maps";
+
+  const texturePaths = {
+    diffuse: {
+      sun: `${diffuseMapDirPath}/sunmap.jpg`,
+      mercury: `${diffuseMapDirPath}/mercurymap.jpg`,
+      venus: `${diffuseMapDirPath}/venusmap.jpg`,
+      earth: `${diffuseMapDirPath}/earthmap.jpg`,
+      moon: `${diffuseMapDirPath}/moonmap.jpg`,
+      mars: `${diffuseMapDirPath}/marsmap.jpg`,
+      jupiter: `${diffuseMapDirPath}/jupitermap.jpg`,
+      saturn: `${diffuseMapDirPath}/saturnmap.jpg`,
+      uranus: `${diffuseMapDirPath}/uranusmap.jpg`,
+      neptune: `${diffuseMapDirPath}/neptunemap.jpg`,
+    },
+    bump: {
+      mercury: `${bumpMapDirPath}/mercurybump.jpg`,
+      venus: `${bumpMapDirPath}/venusbump.jpg`,
+      earth: `${bumpMapDirPath}/earthbump.jpg`,
+      mars: `${bumpMapDirPath}/marsbump.jpg`,
+    },
+    specular: {
+      earth: `${specularMapDirPath}/earthspec.jpg`,
+    },
+  };
+
+  const textureMap = {};
+
+  console.log("Loading textures...");
+  const loadPromises = [];
+  for (const [mapType, maps] of Object.entries(texturePaths)) {
+    textureMap[mapType] = {};
+
+    for (const [mapName, filePath] of Object.entries(maps)) {
+      const loadPromise = new Promise(function (resolve, reject) {
+        const loader = new THREE.TextureLoader();
+
+        console.log(`Loading ${filePath}...`);
+        loader.load(filePath, function (texture) {
+          textureMap[mapType][mapName] = texture;
+          console.log(`${filePath} loaded.`);
+          resolve(texture);
+        });
+      });
+
+      loadPromises.push(loadPromise);
+    }
+  }
+
+  await Promise.all(loadPromises).then(function () {
+    console.log("Finished loading textures.");
+  });
+
+  return textureMap;
 }
 
-function generateIcosahedron() {
-  let geometry = new THREE.Geometry();
+function generateIcosahedron(materialOptions) {
+  const geometry = new THREE.Geometry();
 
   // Calculate the golden ratio.
-  let p = (1.0 + Math.sqrt(5.0)) / 2.0;
+  const p = (1.0 + Math.sqrt(5.0)) / 2.0;
 
-  let vertices = [
-    new THREE.Vector3( 0,  p, -1), // 0
-    new THREE.Vector3( 0,  p,  1), // 1
-    new THREE.Vector3( 0, -p,  1), // 2
-    new THREE.Vector3( 0, -p, -1), // 3
+  const vertices = [
+    new THREE.Vector3(0, p, -1), // 0
+    new THREE.Vector3(0, p, 1), // 1
+    new THREE.Vector3(0, -p, 1), // 2
+    new THREE.Vector3(0, -p, -1), // 3
     // Rectangle 2
-    new THREE.Vector3(-p,  1,  0), // 4
-    new THREE.Vector3( p,  1,  0), // 5
-    new THREE.Vector3( p, -1,  0), // 6
-    new THREE.Vector3(-p, -1,  0), // 7
+    new THREE.Vector3(-p, 1, 0), // 4
+    new THREE.Vector3(p, 1, 0), // 5
+    new THREE.Vector3(p, -1, 0), // 6
+    new THREE.Vector3(-p, -1, 0), // 7
     // Rectangle 3
-    new THREE.Vector3(-1,  0, -p), // 8
-    new THREE.Vector3(-1,  0,  p), // 9
-    new THREE.Vector3( 1,  0,  p), // 10
-    new THREE.Vector3( 1,  0, -p), // 11
+    new THREE.Vector3(-1, 0, -p), // 8
+    new THREE.Vector3(-1, 0, p), // 9
+    new THREE.Vector3(1, 0, p), // 10
+    new THREE.Vector3(1, 0, -p), // 11
   ];
 
-  let faces = [
+  const faces = [
     // Faces centered around vertex 0.
-    new THREE.Face3( 4,  1,  0),
-    new THREE.Face3( 8,  4,  0),
-    new THREE.Face3(11,  8, 0),
-    new THREE.Face3( 5, 11,  0),
-    new THREE.Face3( 1,  5,  0),
+    new THREE.Face3(4, 1, 0),
+    new THREE.Face3(8, 4, 0),
+    new THREE.Face3(11, 8, 0),
+    new THREE.Face3(5, 11, 0),
+    new THREE.Face3(1, 5, 0),
     // Adjacent faces.
-    new THREE.Face3( 4,  9,  1),
-    new THREE.Face3( 8,  7,  4),
-    new THREE.Face3(11,  3,  8),
-    new THREE.Face3( 5,  6, 11),
-    new THREE.Face3( 1, 10,  5),
+    new THREE.Face3(4, 9, 1),
+    new THREE.Face3(8, 7, 4),
+    new THREE.Face3(11, 3, 8),
+    new THREE.Face3(5, 6, 11),
+    new THREE.Face3(1, 10, 5),
     // Faces centered around vertex 2.
-    new THREE.Face3( 3,  6,  2),
-    new THREE.Face3( 7,  3,  2),
-    new THREE.Face3( 9,  7,  2),
-    new THREE.Face3(10,  9,  2),
-    new THREE.Face3( 6, 10,  2),
+    new THREE.Face3(3, 6, 2),
+    new THREE.Face3(7, 3, 2),
+    new THREE.Face3(9, 7, 2),
+    new THREE.Face3(10, 9, 2),
+    new THREE.Face3(6, 10, 2),
     // Adjacent faces.
-    new THREE.Face3( 3, 11,  6),
-    new THREE.Face3( 7,  8,  3),
-    new THREE.Face3( 9,  4,  7),
-    new THREE.Face3(10,  1,  9),
-    new THREE.Face3( 6,  5, 10),
+    new THREE.Face3(3, 11, 6),
+    new THREE.Face3(7, 8, 3),
+    new THREE.Face3(9, 4, 7),
+    new THREE.Face3(10, 1, 9),
+    new THREE.Face3(6, 5, 10),
   ];
 
   // A regular icosahedron can be generated via three orthogonal rectangles
@@ -62,49 +125,66 @@ function generateIcosahedron() {
   geometry.vertices = vertices;
   geometry.faces = faces;
 
-  let material = new THREE.MeshBasicMaterial({ color: 0xee00ee });
-  let mesh = new THREE.Mesh(geometry, material);
+  // Refine icosahedron.
+  const level = 4;
+  for (let i = 0; i < level; i++) {
+    refineIcosahedron(geometry);
+  }
+
+  geometry.computeFaceNormals();
+  geometry.computeVertexNormals();
+
+  const material = new THREE.MeshPhongMaterial(materialOptions);
+  const mesh = new THREE.Mesh(sphere, material);
 
   return mesh;
 }
 
-function refineIcosahedron(mesh) {
-  // Initialize array to hold new subdivided faces.
-  let updatedFaces = [];
+function generateSphere(materialOptions) {
+  const sphere = new THREE.SphereGeometry(2, 64, 64);
+  const material = new THREE.MeshPhongMaterial(materialOptions);
+  const mesh = new THREE.Mesh(sphere, material);
 
-  let faceCount = mesh.geometry.faces.length;
+  return mesh;
+}
+
+function refineIcosahedron(geometry) {
+  // Initialize array to hold new subdivided faces.
+  const updatedFaces = [];
+
+  const faceCount = geometry.faces.length;
   for (let i = 0; i < faceCount; i++) {
-    let face = mesh.geometry.faces[i];
+    const face = geometry.faces[i];
 
     // Get subdivided faces.
-    let newFaces = subdivideFace(face, mesh);
+    const newFaces = subdivideFace(face, geometry);
 
     // Push subdivided faces to new array.
-    let newFacesCount = newFaces.length;
+    const newFacesCount = newFaces.length;
     for (var j = 0; j < newFacesCount; j++) {
       updatedFaces.push(newFaces[j]);
     }
   }
 
   // Replace previous faces with updated ones.
-  mesh.geometry.faces = updatedFaces;
+  geometry.faces = updatedFaces;
 
-  return mesh;
+  return geometry;
 }
 
-function subdivideFace(face, mesh) {
+function subdivideFace(face, geometry) {
   // Retrieve vertices of the given face.
-  let v1 = mesh.geometry.vertices[face.a];
-  let v2 = mesh.geometry.vertices[face.b];
-  let v3 = mesh.geometry.vertices[face.c];
+  const v1 = geometry.vertices[face.a];
+  const v2 = geometry.vertices[face.b];
+  const v3 = geometry.vertices[face.c];
 
-  // Add midpoints of each edge to the mesh.
-  let mid1 = addMidpoint(v1, v2, mesh);
-  let mid2 = addMidpoint(v2, v3, mesh);
-  let mid3 = addMidpoint(v3, v1, mesh);
+  // Add midpoints of each edge to the geometry.
+  const mid1 = addMidpoint(v1, v2, geometry);
+  const mid2 = addMidpoint(v2, v3, geometry);
+  const mid3 = addMidpoint(v3, v1, geometry);
 
   // Create new faces with the new vertices.
-  let newFaces = [
+  const newFaces = [
     new THREE.Face3(face.a, mid1, mid3),
     new THREE.Face3(mid1, face.b, mid2),
     new THREE.Face3(mid3, mid2, face.c),
@@ -122,93 +202,135 @@ function getMidpoint(v1, v2) {
   );
 }
 
-function addMidpoint(v1, v2, mesh) {
-  let midpoint = getMidpoint(v1, v2);
+function addMidpoint(v1, v2, geometry) {
+  const midpoint = getMidpoint(v1, v2);
 
   // Calculate factor to scale new vertex to desired spherical radius.
-  let scalingFactor = v1.length() / midpoint.length();
+  const scalingFactor = v1.length() / midpoint.length();
 
   // Create a scaled midpoint vector.
-  let scaledVector = midpoint.multiplyScalar(scalingFactor);
+  const scaledVector = midpoint.multiplyScalar(scalingFactor);
 
   // Set index for latest vector.
-  let newIndex = mesh.geometry.vertices.length;
+  const newIndex = geometry.vertices.length;
 
-  // Add latest vector to mesh.
-  mesh.geometry.vertices.push(scaledVector);
+  // Add latest vector to geometry.
+  geometry.vertices.push(scaledVector);
 
   return newIndex;
 }
 
-function main() {
+function updateMeshMaterial(mesh, materialOptions) {
+  mesh.material = Object.assign(mesh.material, materialOptions);
+  mesh.material.needsUpdate = true;
+}
+
+function generateMaterialOptions(materialName) {
+  const diffuseMap =
+    materialName in textureMaps.diffuse && textureMaps.diffuse[materialName];
+  const bumpMap =
+    materialName in textureMaps.bump && textureMaps.bump[materialName];
+  const specularMap =
+    materialName in textureMaps.specular && textureMaps.specular[materialName];
+
+  const materialOptions = {};
+  diffuseMap != null && (materialOptions.map = diffuseMap);
+  bumpMap != null && (materialOptions.bumpMap = bumpMap);
+  specularMap != null && (materialOptions.specularMap = specularMap);
+
+  return materialOptions;
+}
+
+async function main() {
+  const config = {
+    highlightEdges: false,
+    highlightVertices: false,
+  };
+
+  textureMaps = await loadTextures();
+
   // Initialize scene.
-  let scene = new THREE.Scene();
-  scene.background = new THREE.Color(0x555555);
+  const scene = new THREE.Scene();
+  scene.background = new THREE.Color(0x404040);
+
+  const sceneObjects = [];
 
   // Initialize camera.
-  let camera = new THREE.PerspectiveCamera(
+  const camera = new THREE.PerspectiveCamera(
     75,
     window.innerWidth / window.innerHeight,
     0.1,
-    1000,
+    1000
   );
 
   // Initialize renderer.
-  let renderer = new THREE.WebGLRenderer();
+  const renderer = new THREE.WebGLRenderer();
   renderer.setSize(window.innerWidth / 2, window.innerHeight / 2);
   document.body.appendChild(renderer.domElement);
 
   // Create polyhedral object.
-  let poly = generateIcosahedron();
+  const materialOptions = generateMaterialOptions(materialName);
+  const poly = generateSphere(materialOptions);
+  sceneObjects.push(poly);
 
   // Move camera to avoid coinciding with the object.
   camera.position.z = 4;
-
-  // Refine icosahedron.
-  let level = 4;
-  for (let i = 0; i < level; i++) {
-    poly = refineIcosahedron(poly);
-  }
 
   // Add polyhedron to the scene.
   scene.add(poly);
 
   // Create edge highlighting.
-  let edgeMaterial = new THREE.LineBasicMaterial(
-    {
+  if (config.highlightEdges) {
+    const edgeMaterial = new THREE.LineBasicMaterial({
       color: 0x00ffff,
       linewidth: 1,
-    }
-  );
-  let edges = new THREE.LineSegments(poly.geometry, edgeMaterial);
+    });
+    const edges = new THREE.LineSegments(poly.geometry, edgeMaterial);
 
-  // Add edge highlighting to scene.
-  scene.add(edges);
+    // Add edge highlighting to scene.
+    scene.add(edges);
+    sceneObjects.push(edges);
+  }
 
   // Create vertex highlighting.
-  let pointMaterial = new THREE.PointsMaterial(
-    {
+  if (config.highlightVertices) {
+    const pointMaterial = new THREE.PointsMaterial({
       color: 0x0000ff,
       size: 5,
       sizeAttenuation: false,
-    }
-  );
-  let points = new THREE.Points(poly.geometry, pointMaterial);
+    });
+    const points = new THREE.Points(poly.geometry, pointMaterial);
 
-  // Add vertex highlighting to scene.
-  scene.add(points);
+    // Add vertex highlighting to scene.
+    scene.add(points);
+    sceneObjects.push(points);
+  }
+
+  const ambientLight = new THREE.AmbientLight(0xffffff);
+  scene.add(ambientLight);
+
+  const directLight = new THREE.DirectionalLight(0xcccccc, 1);
+  directLight.position.set(5, 5, 5);
+  scene.add(directLight);
+
+  // Prepare variables for use in the loop.
+  let currentMaterialName = materialName;
 
   // Run animation loop.
   (function loop() {
     window.requestAnimationFrame(loop);
 
+    if (materialName != currentMaterialName) {
+      const newMaterialOptions = generateMaterialOptions(materialName);
+      poly.material = Object.assign(poly.material, newMaterialOptions);
+      poly.material.needsUpdate = true;
+    }
+
     // Rotate object.
-    poly.rotation.x += 0.01;
-    poly.rotation.y += 0.01;
-    edges.rotation.x += 0.01;
-    edges.rotation.y += 0.01;
-    points.rotation.x += 0.01;
-    points.rotation.y += 0.01;
+    for (const object of sceneObjects) {
+      //object.rotation.x += 0.01;
+      object.rotation.y += 0.01;
+    }
 
     renderer.render(scene, camera);
   })();
